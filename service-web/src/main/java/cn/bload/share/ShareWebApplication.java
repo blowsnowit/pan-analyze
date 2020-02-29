bafded4413fb406e4098bd435e86608ad375699e
bl_it <2426477553@qq.com>
2020-02-29 16:18:12 +0800

    1. 打包配置

--- service-web/src/main/java/cn/bload/share/ShareWebApplication.java
+++ service-web/src/main/java/cn/bload/share/ShareWebApplication.java
@@ -2,11 +2,19 @@ package cn.bload.share;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShareWebApplication {
+public class ShareWebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ShareWebApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        //此处加载的资源为`MainApplication`
+        return builder.sources(ShareWebApplication.class);
     }
 }
